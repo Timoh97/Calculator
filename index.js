cc6f3f3c44c86129bbd7eaf9c2677c44bd2747e4
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-10-03 00:29:52 +0300

    add javascript code for the functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+// This function clears the screen
+// Explanation:The clearScreen() function access the DOM using the id of the result 
+// and clear its value by assigning it an empty string.
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+   }
+
+
+   // This function display values
+   //display() function accesses the DOM using the id of the result 
+   //and appends the value of the clicked button to the result.
+function display(value) {
+    document.getElementById("result").value += value;
+   }
+
+
+   // This function evaluates the expression and return result
+   //The calculate() function accesses the DOM using the id of the result and evaluates the expression using the eval() function. 
+   //The evaluated value of the expression is again assigned to the result.
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+   }
